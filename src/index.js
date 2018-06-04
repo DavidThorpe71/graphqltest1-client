480a68c05a88b6c134a11fb767198bf1bb0d74e3
David Thorpe <davidthorpe878@gmail.com>
2018-06-04 20:54:55 +0100

    First request setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const httpLink = new HttpLink({
+  uri: '/graphql'
+})
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+})
+
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
+
+ReactDOM.render(<ApolloApp />, document.getElementById('root'));
 registerServiceWorker();
