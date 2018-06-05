76ad786ba324749f8e5750862044297b8f134676
David Thorpe <davidthorpe878@gmail.com>
2018-06-05 22:36:13 +0100

    added comment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import ResolutionForm from './components/ResolutionForm';
 
 const App = ({ data }) => {
   if (data.loading) return null;
   return (
     <div>
-      {data.resolutions.map(resolution => (
-        <h3 key={resolution._id}>{resolution.name}</h3>
-      )
-      )}
+      <ResolutionForm />
+      <div className="resolutions-list">
+        {data.resolutions.map(resolution => (
+          <h3 key={resolution._id}>{resolution.name}</h3>
+        )
+        )}
+      </div>
     </div>
   )
 }
